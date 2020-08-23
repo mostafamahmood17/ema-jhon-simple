3809cde38841e4ecad1752fb01a851bc4e118626
mostafamahmood17 <taat.heritage@gmail.com>
2020-08-23 02:25:29 -0700

    module 34

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData';
+import './Shop.css'
+import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
+
+
+
+const Shop = () => {
+    const firstTen = fakeData.slice(0,10);
+    const [products,setProducts] = useState(firstTen);
+    const [cart , setCart]= useState([]);
+
+   const handleAddProduct = (product)=>{
+       console.log("product added", product)
+       const newCart = [...cart , product];
+       setCart(newCart);
+   }
+    return (
+        <div className="shop-container">
+            <div className="product-container"> 
+                   {                   
+                        products.map(pd => 
+                        <Product handleAddProduct = {handleAddProduct} product={pd}>
+                        </Product>)
+                    }
+            </div>
+            <div className="cart-container">
+               <Cart cart={cart}></Cart>
+            </div>
+           
+        </div>
+    );
+};
+
+export default Shop;
